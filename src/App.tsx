db817f38ce4ebe54614a766191e2278845e6c228
marriamdd <davitashvili.m3@gmail.com>
2024-05-03 17:18:45 +0400

    doc:prepared project with besics

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Settings from "./components/Settings";
+import Timer from "./components/Timer";
+
+function App() {
+  return (
+    <>
+      <Timer />
+      <Settings />
+    </>
+  );
+}
+
+export default App;
